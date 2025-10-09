36d181992b1710d408e22cfc10c9a2df2324a665
takver <ellietakver@gmail.com>
2025-10-09 15:25:34 -0300

    Adjusting database

--- ViCosmuz/settings.py
+++ ViCosmuz/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'ViCosmuz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vicosmus',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
